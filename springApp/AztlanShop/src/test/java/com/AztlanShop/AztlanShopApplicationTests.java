1772b1ad2a375f18b5f476b2d6fc31eae4c48cd4
guateCrazyDev <leonardogt2099@gmail.com>
2026-03-20 17:58:14 -0600

    Adding spring app react app and docker configuration

---
+++ springApp/AztlanShop/src/test/java/com/AztlanShop/AztlanShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AztlanShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AztlanShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
